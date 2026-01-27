67273b07dc6c7603f375ebcf0a0e0f691099ce4a
Trinh Doan <trinhd997@gmail.com>
2026-01-27 12:33:13 +0200

    based codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState, useEffect} from 'react';
+import './App.css';
+import extensions from "./data.json" with {type: "json"};
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [themeImg,setThemeImg]=useState("/icon-moon.svg");
+    /*extensions.splice(0,0);*/
+    return (
+        <>
+            <div className="container__header">
+                <h1><img src="/logo.svg" /></h1>
+                <button className="wide-btn">
+                    <img src={themeImg} alt="theme image"/>
+                </button>
+            </div>
+            <div className="container__main-content">
+                <h2>Extensions List</h2>
+                <div className="container__filters">
+                    <button className="wide-btn">All</button>
+                    <button className="wide-btn">Active</button>
+                    <button className="wide-btn">Inactive</button>
+                </div>
+                <ul>
+                    {
+                        extensions.map((data) => {
+                            return (
+                                <li key={data["name"]}>
+                                    <div>
+                                        <img src={data["logo"]} alt="extension logo" />
+                                        <h3>{data["name"]}</h3>
+                                        <p>{data["description"]}</p>
+                                        <input type="checkbox" id="btnToggle" name="btnToggle" />
+                                        <button className="wide-btn">Remove</button>
+                                    </div>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default App
